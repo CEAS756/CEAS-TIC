6e777f6fbed6553b011171b88ff4bec14f6dfb50
CEAS756 <mamatamondal706348@gmail.com>
2026-06-09 19:43:02 +0530

    Delete src directory

--- src/events/inviteCreate.js
+++
@@ -1,9 +0,0 @@
-const { upsertInvite } = require('../utils/database');
-
-module.exports = {
-  name: 'inviteCreate',
-  async execute(invite, client) {
-    upsertInvite(invite.code, invite.guild.id, invite.inviter?.id || 'unknown', invite.uses || 0);
-    console.log(`[INVITE] New invite ${invite.code} created by ${invite.inviter?.tag}`);
-  },
-};
